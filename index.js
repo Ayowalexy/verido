f6b5c3fb93aa41f7dc453d84f012d3f104febbd2
Ayowalexy <seinde4@gmail.com>
2021-12-23 20:04:37 +0100

    webhook

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const DATABASE = process.env.DATABASE
 
 const DB = `mongodb+srv://seinde4:${PASSWORD}@cluster0.pp8yv.mongodb.net/${DATABASE}?retryWrites=true&w=majority` || 'mongodb://localhost:27017/verido';
 
-mongoose.connect('mongodb://localhost:27017/verido',
+mongoose.connect(DB,
     {    
     useNewUrlParser: true,
     useUnifiedTopology: true,
